442e2a6c4f03be3d592be4d6707be21d2bd9ca55
trigger <triggeruseromar@gmail.com>
2021-07-19 13:18:29 -0500

    second

--- tsa_sitio/settings.py
+++ tsa_sitio/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'tsa_sitio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DB_TSA',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'USER': 'root',
-        'PASSWORD': 'trigger',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
